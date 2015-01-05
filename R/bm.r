81e4958f2f2df0f7d8ce57d4edb6e25281200e62
David Winter <david.winter@gmail.com>
2015-01-05 10:44:11 -0700

    BM estimators take similar input to dma_* fxns
    As of now, inputs are vector of fitnesses from which delta_m can be calculated,
    and an estimate of Ve, so delta_v can be calcuated from the fitnesses

--- R/bm.r
+++ R/bm.r
@@ -1,15 +1,18 @@
 ##' Bateman-mukai estimators for MA data
 ##'@export
-##'@param delta_m numeric, change in mean fitness
-##'@param delta_v numeric, change in among-line variance
+##'@param obs, a vector of ftiness values
+##'@param Ve, an estimate of within-line ('environmental') variance
 ##'@return U numeric BM estimator of mutation rate
 ##'@return Ea numeric BM estimator of effect-size
 ##'@examples
-##' BM(-0.01, 0.001)
-##' x <- rma_normal(n=100, s=0.01, Vs=1e-9, Ve=1e-3, Ut=10)
-##' BM(mean(x), var(x) - 1e-3)
-
-BM <- function(delta_m, delta_v){
+##'
+##' low_var <- rma_normal(n=100, s=0.01, Vs=1e-9, Ve=1e-4, Ut=10)
+##' BM(low_var, 1e-4)
+##' high_var <- rma_normal(n=100, s=0.01, Vs=1e-3, Ve=1e-4, Ut=10)
+##' BM(high_var, 1e-4)
+BM <- function(obs, Ve){
+    delta_m <- mean(obs)
+    delta_v <- var(obs) - Ve
     return(list(U=(2*delta_m^2)/delta_v, Ea = delta_v/delta_m))
 }
 
