cabaee5d3fcd2435b402a503f59b59f48c96f499
David Winter <david.winter@gmail.com>
2015-07-02 11:21:22 -0700

    Moar IG
    Added random generating, density and fitting functions for
    the inv-gamma MA models with no known mutations

--- R/inverse_gaussian.r
+++ R/inverse_gaussian.r
@@ -7,16 +7,83 @@ dinverse_gaussian <- function(x, mean, shape){
 
 #'@export
 rinverse_gaussian <- function(n, mean, shape){
-    mean <- u
+    u <- mean
     y <- rnorm(n, 0, 1)**2
-    left <-  u + (u**2*y)/(2*shape) 
-    right <-  (u / 2*shape) * sqrt(4*u*shape*y + u**2*y**2)
+#    left <- mean + (mean*mean*y)/(2*shape) 
+#    right <-  (mean/(2*shape)) * sqrt(4*mu*lambda*y + mu*mu*y*y)
+    left <-  mean + (u**2*y)/(2*shape) 
+    right <-  (mean / (2*shape)) * sqrt(4*mean*shape*y + mean**2*y**2)
     x <- left-right
     z <- runif(n, 0, 1)
-    ifelse(z <= u/(u+x), x, u**2/x)
+    ifelse(z <= mean/(mean+x), x, mean**2/x)
 }
-    
+ 
 
-    
+rma_IG <- function(n, mean, shape, Ve, Ut){
+    k <- rpois(n,Ut)
+    between_line <- rnorm(n, 0, sqrt(Ve))
+    mutations <- rinverse_gaussian(n, k*mean, k**2*shape)
+    #deal with NAs cause by zero mutation cases
+    ifelse(k > 0, between_line + mutations, between_line)    
+}
 
+
+#TODO vectorize this and NIG convolution (maybe in C++)
+.dma_IG_one <- function(w, mean, shape, Ve, Ut, log=FALSE){
+    p_mu <- mu_scan(Ut)
+    n <- length(p_mu) -1
+    res <-  sum(sapply(0:n,  dfe:::NIG_convolution, z=w, mean=mean, shape=shape, Ve=Ve, verbose=FALSE) * p_mu)
+    if(log){
+        return(log(res))
+    }
+    res
+}
+
+dma_IG<- function(w, mean, shape, Ve, Ut, log=FALSE){
+    res <- sum(vapply(w, .dma_IG_one, mean=mean, shape=shape, Ve=Ve, Ut=Ut, log=TRUE, FUN.VALUE=0.0))
+    if(log){
+        return(res)
+    }
+    exp(res)
+}
+
+fit_ma_IG <- function(obs, fixed=list(), start=list(), verbose=FALSE){
+    all_args <- list("mean", "shape", "Ve","Ut")
+    known_args <- c(names(fixed), names(start))
+    if(!all(all_args %in% known_args)){
+        stop("Specify all the args yo")
+    }
+    Q <- function(mean, shape, Ve, Ut){
+        if(any( c(shape, mean, Ve, Ut) <= 0)){
+           return(999999)
+        }
+        res <- -dma_IG(obs, mean, shape, Ve, Ut,log=TRUE)
+        if(verbose){
+            params <- match.call()
+            print (sapply(c(as.list(params)[2:5],res), round, 4))
+        }
+        res
+    }
+      
+    mle(Q, start=start, fixed=fixed,
+        method="L-BFGS-B",
+        lower=rep(1e-6,length(start)))
+        
+}
+
+
+
+NIG_convolution <- function(z, mean, shape, Ve, k, verbose=FALSE){
+    if(k==0){
+        return(dnorm(z, 0, sqrt(Ve)))
+    }
+    res <- integrate(
+      function(x,y) dnorm(y-x, 0, sqrt(Ve)) * dinverse_gaussian(x, mean=mean*k, shape=shape*k**2),
+      y=z, lower=0, upper=Inf, abs.tol=1e-7
+    )
+    if(verbose){
+        return(res)
+    }
+    res$value
+}
 
