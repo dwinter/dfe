3f015e1f6cea10efa5c5473852983d3144dc648b
David Winter <david.winter@gmail.com>
2015-09-15 16:03:58 -0700

    change FGM function to match most commom paramaterization

--- R/fgm.r
+++ R/fgm.r
@@ -22,29 +22,33 @@
 
 rma_FGM <- function(n, start, Vm, Ve, Ut, FUN=gaussian_fitness){
     nloci <- length(start)
-    if(length(Ve==1) & nloci > 1){
-        idm <- diag(length(start))
-        diag(idm) <- Ve
-        Ve <- idm
+    if(length(Vm == 1) & nloci > 1){
+        Vm <- make_sigma_mat(Vm, nloci)
     }
-#    starting_fitness <- FUN(start)
     k <- rpois(n, Ut)
-    experimental_variance <- rnorm(n, 0,Ve)
-
-    mutate <- function(k){
-        if(k==0){
-           return(start)
-        }
-        else{
-            mutations <- MASS::mvrnorm(k, rep(0, nloci), Ve)
-            moves <- if(k==1) sum(mutations) else colSums(mutations)
-            return(start + moves)
-        }
-    }
-    new_positions <- vapply(k, mutate, FUN.VALUE=numeric(nloci))
+    experimental_variance <- rnorm(n, 0, sqrt(Ve))
+    new_positions <- vapply(k, mutate, start=start, Vm=Vm, FUN.VALUE=numeric(nloci))
     ending_fitnesses <- apply(new_positions, 2, FUN, start=start) + experimental_variance
     #return( ending_fitnesses - starting_fitness)
     -ending_fitnesses
+}
+
+make_sigma_mat <- function(Vm, nloci){
+    idm <- diag(nloci)
+    diag(idm) <- Vm
+    Vm <- idm
+    Vm
+}
+
+mutate <- function(start, Vm,k){
+    if(k==0){
+        return(start)
+    } 
+    mutations <- MASS::mvrnorm(k, start, sqrt(Vm))
+    if(k >1){
+       mutations <-  colSums(mutations)
+    }
+    mutations
 }
 
 gaussian_fitness <- function(coords, start, optimum="origin"){
@@ -75,19 +79,28 @@ sq_dist <- function(coords, optimum="origin"){
 #' @param FUN, function, fitness funciton
 #' @param plot, boolean, make density plot of dfe (default=FALSE)
 #' @export
-estimate_FGM_moments <- function(n=1e5, start, Vm, FUN=sq_dist, plot=FALSE){
-    sims <- replicate(n, start + rnorm(length(start), 0, sqrt(Vm)))
-    w <-  apply(sims, 2, FUN) - FUN(start)
+estimate_FGM_moments <- function(n=1e4, start, Vm, FUN=gaussian_fitness, plot=TRUE){
+    nloci <- length(start)
+    if(length(Vm == 1) & nloci > 1){
+        Vm <- make_sigma_mat(Vm, nloci)
+    }
+    k <- rep(1,n )
+    new_positions <- vapply(k, mutate, start=start, Vm=Vm, FUN.VALUE=numeric(nloci))
+    w <- -apply(new_positions, 2, FUN, start=start) 
+    #return( ending_fitnesses - starting_fitness)
     mean_sim <- mean(w)
     var_sim <- var(w)
+    den <- density(w)
     if(plot){
-        plot(density(w))
+        plot(den)
     }
-
     return(list(mean        =  mean_sim ,
                 variance    =  var_sim, 
                 skew_median =  (3*(mean_sim - median(w)))/ sqrt(var_sim),
-                p_benifical = mean(w <0)))
+                p_benifical = mean(w <0),
+                density     = den
+                )
+           )
     
 }
 
