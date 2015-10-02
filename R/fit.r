24eb0f75bfe35eb8a0b8ce65b7c9794f318139fc
David Winter <david.winter@gmail.com>
2015-10-02 09:46:02 -0700

    specify method arguments in otpmix calls

--- R/fit.r
+++ R/fit.r
@@ -1,27 +1,27 @@
-#
-#mom_and_ll <-  function(w, Ve, k, p_n){
-#  mom_est <- mom_ma_gamma(w, Ve, mean ( k ) * (1-p_n)  )
-#  shape_mom <- mom_est[["shape"]]
-#  rate_mom <- mom_est[["rate"]]
-#
-#  LL <- dma_gamma_known(w, Ve=Ve, shape=shape_mom,  rate=rate_mom, p_neutral = p_n, k=k)
-#  c( LL_mom=LL, p_n=p_n, shape_mom = shape_mom, rate_mom = rate_mom )
-#}
-#
-#
-#mom_and_maxamize <- function(w, Ve, k, p_n){
-#    res <- mom_and_ll(w, Ve, k, p_n)
-#    mod <- fit_gamma_known(w, 
-#                           start=list(shape=res[["shape_mom"]], rate=res[["rate_mom"]]),
-#                           fixed=list(Ve=Ve, p_neutral = p_n), k)
-#    c(res, mod@coef, maxLL=-mod@min)
-#                           
-#}
-#
-#
-#
-#AIC <- function(ests)  6- 2 *ests[5,]
-#ests <- sapply(Ut, mom_and_ll, w=w, Ve=0.001)
+
+mom_and_ll <-  function(w, Ve, k, p_n){
+  mom_est <- mom_ma_gamma(w, Ve, mean ( k ) * (1-p_n)  )
+  shape_mom <- mom_est[["shape"]]
+  rate_mom <- mom_est[["rate"]]
+
+  LL <- dma_gamma_known(w, Ve=Ve, shape=shape_mom,  rate=rate_mom, p_neutral = p_n, k=k)
+  c( LL_mom=LL, p_n=p_n, shape_mom = shape_mom, rate_mom = rate_mom )
+}
+
+
+mom_and_maxamize <- function(w, Ve, k, p_n){
+    res <- mom_and_ll(w, Ve, k, p_n)
+    mod <- fit_gamma_known(w, 
+                           start=list(shape=res[["shape_mom"]], rate=res[["rate_mom"]]),
+                           fixed=list(Ve=Ve, p_neutral = p_n), k)
+    c(res, mod@coef, maxLL=-mod@min)
+                           
+}
+
+
+
+AIC <- function(ests)  6- 2 *ests[5,]
+ests <- sapply(Ut, mom_and_ll, w=w, Ve=0.001)
 
 
 
@@ -72,9 +72,9 @@ make_dfe_fitting_fxn <- function(Q, all_params, verbose=TRUE, lower=list(), uppe
             return( optimx(par=unlist(start), fn=LL, lower = lower_bound, method = "L-BFGS-B", ...) )
         }
         if(is.null(lower_bound)){
-            return( optimx(unlist(start), fn=LL, upper=upper_bound, "L-BFGS-B", ...) )
+            return( optimx(unlist(start), fn=LL, upper=upper_bound, method= "L-BFGS-B", ...) )
         }
-        optimx(unlist(start), LL, lower=lower_bound, upper=upper_bound, "L-BFGS-B", ...)
+        optimx(unlist(start), fn=LL, lower=lower_bound, upper=upper_bound, method="L-BFGS-B", ...)
     }
     f
 }
@@ -89,13 +89,14 @@ fit_gamma_known <- make_dfe_fitting_fxn(dma_gamma_known,
     
 
 
-#mu <- rpois(100,10)
-#
-#w <- rma_known_gamma(shape=1, rate=10, Ve=0.01, k=mu, p_neutral=0.6)
-#mod <- fit_gamma_known(w, 
-#                       fixed=list(Ve=0.01, shape=1, rate=10),
-#                       start=list(p_neutral = 0.1),
-#                       k=mu)
-#
-#pn <- seq(0.1, 0.9, 0.05)
-#res <- vapply(pn, mom_and_ll, w=w, Ve=0.01, k=mu, FUN.VALUE=rep(0.01,4))
+mu <- rpois(100,10)
+
+w <- rma_known_gamma(shape=1, rate=10, Ve=0.01, k=mu, p_neutral=0.6)
+mod <- fit_gamma_known(w, 
+                       fixed=list(Ve=0.01, shape=1, rate=10, k=mu),
+                       start=list(p_neutral = 0.1) )
+                       
+
+
+pn <- seq(0.1, 0.9, 0.05)
+res <- vapply(pn, mom_and_ll, w=w, Ve=0.01, k=mu, FUN.VALUE=rep(0.01,4))
